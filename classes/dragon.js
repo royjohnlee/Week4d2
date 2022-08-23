f36b2165561f27c1562727f26e3c191f85f56706
royjohnlee <royjohnlee@DESKTOP-BO5FP47.localdomain>
2022-08-23 14:51:44 -0700

    dragon

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,30 @@
 // Your code here
 
+class Dragon {
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+
+  static getDragons(...dragon) {
+
+    console.log(dragon)
+    let newArr = []
+    for (let ele of dragon) {
+      newArr.push(ele.name)
+    }
+    return newArr
+  }
+}
+
+
+
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
